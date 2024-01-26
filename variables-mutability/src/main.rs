365dac67397217bea1e948b4f701acf67ceded73
Douglas Poma <douglaspoma@yahoo.com>
2024-01-26 10:12:09 -0300

    feat: data types

---
+++ variables-mutability/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Estáticamente tipada com inferência de tipos
+    // Escalares: integer, float, boolean, char
+    // integer: unsigned (não aceita valores negativos "u23") e signed (aceita valores negativos "i23")
+    // float: f32 e f64
+    // boolean: true e false
+    // char: qualquer valor dentro de aspas simples(Rust usa Unicode por padrão ao invés de ASCII como as outras linguagens)
+
+    // Compostos: tuple e array (ambos são fixos)
+    // tuple: pode conter valores de tipos diferentes (ex: (i32, f64, char, u8))
+    // array: pode conter valores de um único tipo (ex: [i32; 5] = [1, 2, 3, 4, 5])
+}
