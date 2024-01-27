ac17ee145144eb7cac91c699d0348a3dc1d3ab74
Douglas Poma <douglaspoma@yahoo.com>
2024-01-27 09:37:21 -0300

    feat: varaibles and mutability

--- variables-mutability/src/main.rs
+++ variables-mutability/src/main.rs
@@ -1,3 +1,7 @@
+fn type_of<T>(_variavel: T) -> &'static str {
+    return std::any::type_name::<T>();
+}
+
 fn main() {
     // Estáticamente tipada com inferência de tipos
     // Escalares: integer, float, boolean, char
@@ -9,4 +13,16 @@ fn main() {
     // Compostos: tuple e array (ambos são fixos)
     // tuple: pode conter valores de tipos diferentes (ex: (i32, f64, char, u8))
     // array: pode conter valores de um único tipo (ex: [i32; 5] = [1, 2, 3, 4, 5])
+    let inteiro = 10;
+    let float = inteiro as f64;
+    let string = float.to_string();
+    // let new_float: f64 = string.into();
+    let new_float: f64 = string.parse().unwrap();
+    let _new_inteiro = new_float as i32;
+    let boolean = true;
+    let bool_string: String = boolean.to_string();
+    let _new_boolean: bool = bool_string.parse().unwrap();
+    let bool_from_value: bool = "true".parse().expect("Erro ao converter para boolean");
+    println!("type_of(inteiro): {}", type_of(inteiro));
+    println!("type_of(bool_from_value): {}", type_of(bool_from_value));
 }
