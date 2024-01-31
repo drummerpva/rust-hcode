eef810963ef580b23fd69e280708de04c41ee0a2
Douglas Poma <douglaspoma@yahoo.com>
2024-01-31 08:48:26 -0300

    feat: using extern modules(crate)

--- modulos/src/main.rs
+++ modulos/src/main.rs
@@ -1,7 +1,10 @@
 mod operacoes;
 use operacoes::matematica::*;
+use rand::random;
 
 fn main() {
     println!("2 + 2 : {}", somar(2, 2));
     println!("2 - 2 : {}", subtrair(2, 2));
+
+    println!("Número aletório: {}", random::<i8>());
 }
