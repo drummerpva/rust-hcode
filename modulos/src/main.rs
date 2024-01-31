7fb69678ad8677b746e4b5caa0d4100a4d140c6b
Douglas Poma <douglaspoma@yahoo.com>
2024-01-31 08:38:02 -0300

    feat: modules in other file

--- modulos/src/main.rs
+++ modulos/src/main.rs
@@ -1,11 +1,4 @@
-mod matematica {
-    pub fn somar(a: i32, b: i32) -> i32 {
-        return a + b;
-    }
-    pub fn subtrair(a: i32, b: i32) -> i32 {
-        return a - b;
-    }
-}
+mod matematica;
 
 fn main() {
     println!("2 + 2 : {}", matematica::somar(2, 2));
---
+++ modulos/src/matematica.rs
@@ -0,0 +1,6 @@
+pub fn somar(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+pub fn subtrair(a: i32, b: i32) -> i32 {
+    return a - b;
+}
